0c18d77d7016db83c5174b1a240008554eb8b02f
AKSUF <abu053125@gmail.com>
2023-04-25 07:00:17 +0600

    version4

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const INCREMENT = 'INCREMENT';
+// Constants for Redux actions INCREMENT, DECREMENT, and ADDUSER.const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const ADDUSER = 'ADDUSER';
 //state
+/* An object containing an initial state for a Redux store, with one user object with the name 'anisul' */
 const initialcounterState = {
-    users: [
-        { name: 'anisul' }
-    ]
-}
+        users: [
+            { name: 'anisul' }
+        ]
+    }
+    // Initial state object for user count with initial count of zero
 const initialUserState = {
         count: 0,
     }
-    //action//increment counter
-
-//action -object-type,payload
+    // This is a Redux action creator
+    // function that returns an action object with type INCREMENT.
 const incrementCounter = () => {
     return {
         type: INCREMENT,
     };
 }
+
+// These are action creator functions
+// for decrementing and incrementing the counter.
 const decrementCounter = () => {
     return {
         type: DECREMENT,
     };
 }
+
+
+// This code defines a Redux action creator
+// function
+// for decrementing the counter and a reducer
+// function to handle state changes
+// for INCREMENT and DECREMENT actions.
 const counterReducer = (state = initialcounterState, action) => {
     switch (action.type) {
         case INCREMENT:
