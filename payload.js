7f22f6cd44037a43e395cecea742fa572e212c3c
AKSUF <abu053125@gmail.com>
2023-04-25 17:47:38 +0600

    5thvideo

--- payload.js
+++ payload.js
@@ -2,76 +2,31 @@
 const { createStore } = require("redux");
 
 // Defining action types
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const RESET = 'RESET';
-const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE';
+const ADD_USER = 'ADD_USER';
+
 // Defining initial state
 const initialState = {
-    count: 2,
-}
-
-// Defining an action
-{
-    type: INCREMENT
-
+    users: ['abu'],
+    count: 1,
 }
 
 // Creating an action creator for increment
-const incrementCounterAction = () => {
+const addUser = (user) => {
     return {
-        type: INCREMENT,
+        type: ADD_USER,
+        payload: user,
     };
 }
-
-// Creating an action creator for decrement
-const decrementCounterAction = () => {
-    return {
-        type: DECREMENT,
-    };
-}
-
-// Creating an action creator for reset
-const resetCounterAction = () => {
-    return {
-        type: RESET,
-    };
-};
-// Creating an action creator for reset
-const incrementCounterByValue = (value) => {
-    return {
-        type: INCREMENT_BY_VALUE,
-        payload: value,
-    };
-};
-
 
 // Defining reducer
-const counterReducer = (state = initialState, action) => {
+const userReducer = (state = initialState, action) => {
     // Using a switch case to handle different types of actions
     switch (action.type) {
-        case INCREMENT:
+        case ADD_USER:
             return {
-                ...state,
+                users: [...state.users, action.payload],
                 count: state.count + 1,
             };
-        case DECREMENT:
-            return {
-                ...state,
-                count: state.count - 1,
-            };
-        case RESET:
-            return {
-                ...state,
-                count: 0,
-            };
-        case INCREMENT_BY_VALUE:
-            return {
-                ...state,
-                count: state.count + action.payload,
-            };
-
-
         default:
             // If the action type is not recognized, return the state unchanged
             state;
@@ -79,17 +34,12 @@ const counterReducer = (state = initialState, action) => {
 }
 
 // Creating a store by passing the reducer to createStore function
-const store = createStore(counterReducer);
+const store = createStore(userReducer);
 
 // Adding a subscriber to the store to log state changes
 store.subscribe(() => {
     console.log(store.getState());
 })
 
-
-// Dispatching actions to the store
-// store.dispatch(incrementCounterAction());
-// store.dispatch(decrementCounterAction());
-// store.dispatch(resetCounterAction());
-store.dispatch(incrementCounterByValue(5));
-store.dispatch(incrementCounterByValue(10));
+store.dispatch(addUser("rafiq"));
+store.dispatch(addUser("sagor"));
