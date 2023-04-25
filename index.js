897741b5c9411c419e24091d4aa153e921f9c74f
AKSUF <abu053125@gmail.com>
2023-04-25 07:53:29 +0600

    version1

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
-// Constants for Redux actions INCREMENT, DECREMENT, and ADDUSER.const INCREMENT = 'INCREMENT';
+const { createStore } = require("redux");
+
+// Constants for Redux actions INCREMENT, DECREMENT, and ADDUSER.
+const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const ADDUSER = 'ADDUSER';
-//state
-/* An object containing an initial state for a Redux store, with one user object with the name 'anisul' */
-const initialcounterState = {
-        users: [
-            { name: 'anisul' }
-        ]
-    }
-    // Initial state object for user count with initial count of zero
+
+// Initial state object for counter with initial count of zero
+const initialCounterState = {
+    count: 0,
+};
+
+// An object containing an initial state for a Redux store, with one user object with the name 'anisul'
 const initialUserState = {
-        count: 0,
-    }
-    // This is a Redux action creator
-    // function that returns an action object with type INCREMENT.
+    users: [
+        { name: 'anisul' }
+    ],
+};
+
+// This is a Redux action creator function that returns an action object with type INCREMENT.
 const incrementCounter = () => {
     return {
         type: INCREMENT,
     };
-}
+};
 
-// These are action creator functions
-// for decrementing and incrementing the counter.
+// These are action creator functions for decrementing and incrementing the counter.
 const decrementCounter = () => {
     return {
         type: DECREMENT,
     };
-}
+};
 
-
-// This code defines a Redux action creator
-// function
-// for decrementing the counter and a reducer
-// function to handle state changes
-// for INCREMENT and DECREMENT actions.
-const counterReducer = (state = initialcounterState, action) => {
+// This code defines a Redux reducer function to handle state changes for INCREMENT and DECREMENT actions.
+const counterReducer = (state = initialCounterState, action) => {
     switch (action.type) {
         case INCREMENT:
             return {
                 ...state,
-                count: state.count + 1
+                count: state.count + 1,
             };
         case DECREMENT:
             return {
                 ...state,
-                count: state.count - 1
+                count: state.count - 1,
             };
         default:
-            break;
-
+            return state;
     }
-}
+};
+
+// Create Redux store
+const store = createStore(counterReducer);
+
+// Subscribe to store updates
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// Dispatch actions
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
