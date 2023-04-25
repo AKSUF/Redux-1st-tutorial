7849a1f69d941da87e036d2be929d74419d2e21b
AKSUF <abu053125@gmail.com>
2023-04-25 17:21:38 +0600

    value

--- payload.js
+++ payload.js
@@ -2,10 +2,10 @@
 const { createStore } = require("redux");
 
 // Defining action types
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
-const RESET = 'RESET'
-
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE';
 // Defining initial state
 const initialState = {
     count: 2,
@@ -37,6 +37,14 @@ const resetCounterAction = () => {
         type: RESET,
     };
 };
+// Creating an action creator for reset
+const incrementCounterByValue = (value) => {
+    return {
+        type: INCREMENT_BY_VALUE,
+        payload: value,
+    };
+};
+
 
 // Defining reducer
 const counterReducer = (state = initialState, action) => {
@@ -57,6 +65,13 @@ const counterReducer = (state = initialState, action) => {
                 ...state,
                 count: 0,
             };
+        case INCREMENT_BY_VALUE:
+            return {
+                ...state,
+                count: state.count + action.payload,
+            };
+
+
         default:
             // If the action type is not recognized, return the state unchanged
             state;
@@ -71,7 +86,10 @@ store.subscribe(() => {
     console.log(store.getState());
 })
 
+
 // Dispatching actions to the store
-store.dispatch(incrementCounterAction());
-store.dispatch(decrementCounterAction());
-store.dispatch(resetCounterAction());
+// store.dispatch(incrementCounterAction());
+// store.dispatch(decrementCounterAction());
+// store.dispatch(resetCounterAction());
+store.dispatch(incrementCounterByValue(5));
+store.dispatch(incrementCounterByValue(10));
